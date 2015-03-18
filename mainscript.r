c52f317722b58f5b7af0f1501aa443511ad67de9
Carl Ambroselli <git@carl-ambroselli.de>
2015-03-18 11:34:48 +0100

    switch to local

--- mainscript.r
+++ mainscript.r
@@ -13,10 +13,18 @@ setwd(tmpFolder)
 file.copy('../../layout/main.Rhtml', 'main.Rhtml')
 file.copy('../../layout/main.css', 'main.css')
 
+report.input <- c("HalloTest")
+report.input["requestDiagram"] <- "InputTest"
+report.output <- list()
+
+# Set environment
+knitrenv <- new.env()
+assign("input", report.input, knitrenv)
+
 # Generate File
 fileName <- 'main.html'
-knit('../../layout/main.Rhtml', output=fileName)
+knit('../../layout/main.Rhtml', output=fileName, envir=knitrenv)
 
 # Output
 report.output[["Report"]] <- readChar(fileName, file.info(fileName)$size)
-report.output[[1]] <- report.input["Model Patient set"]
+# report.output[[1]] <- report.input["Model Patient set"]
